48379448cb20558c7e379ab536594273ade78d0e
homam <homamdaas11@gmial.com>
2024-06-16 23:15:36 +0300

    get go flights, start book go flight

--- routes/Flights/flights.php
+++ routes/Flights/flights.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\FlightGo\bookFlightGo;
 use App\Http\Controllers\FlightGo\flightsGo;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('flightsGo',[flightsGo::class,'Flights']);
+Route::get('bookGoFlight',[bookFlightGo::class,'book']);
